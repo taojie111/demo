6cd73120f9532ddf3eb538353eb2d002297b8d87
taojie <1527938655@qq.com>
2021-12-22 17:32:42 +0800

    1

--- src/main/java/demo/pecs/PECSDemo.java
+++ src/main/java/demo/pecs/PECSDemo.java
@@ -13,7 +13,7 @@ public class PECSDemo {
 
     public static void main(String[] args) {
         PECSDemo pecsDemo = new PECSDemo();
-        pecsDemo.m2();
+        pecsDemo.m3();
     }
 
     // Collections.copy方法测试
@@ -27,6 +27,7 @@ public class PECSDemo {
         System.out.println(list2);
     }
 
+    // pecs
     public void m2() {
         List<Apple> pList = new ArrayList<>();
         pList.add(new Apple(1));
@@ -39,5 +40,34 @@ public class PECSDemo {
         List<? super Fruits> consumer = cList;
         consumer.add(new Apple(3));
         System.out.println(consumer);
+    }
+
+    // List、List<Object>、List<?>的区别
+    public void m3() {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+
+        // List
+        List list1 = new ArrayList();
+        list1.add(new Apple(1));
+        list1.add(1);
+        list1.add("a");
+        System.out.println(list1.get(2));
+
+        // List<Object>
+        List<Object> list2 = new ArrayList<>();
+        // List赋值List<Object>，可以赋值，不会报错
+        list2 = list1;
+        // 其他泛型类型赋值，会报错
+        // list2 = integerList;
+
+        // List<?>
+        List<?> list3 = new ArrayList<>();
+        // 添加元素报错
+        // list3.add(new Apple(1));
+        // 添加null不会报错
+        list3.add(null);
+        // 只能通过赋值引用
+        list3 = list1;
     }
 }
