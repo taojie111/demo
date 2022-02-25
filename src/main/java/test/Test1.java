10b081fd18bd95361ec0656cc8a93e878cace211
taojie <1527938655@qq.com>
2022-02-25 14:17:36 +0800

    1

--- src/main/java/test/Test1.java
+++ src/main/java/test/Test1.java
@@ -2,6 +2,8 @@ package test;
 
 public class Test1 {
     public static void main(String[] args) {
-
+        String s = " , , ";
+        String[] split = s.split(",");
+        System.out.println(s);
     }
 }
